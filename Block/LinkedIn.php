2fa180085b3629dd2be1bcb45f132924496bad55
Vinay Shah <vinay.sudhir@gmail.com>
2018-04-15 21:31:17 +0530

    linked in profile update

--- Block/LinkedIn.php
+++ Block/LinkedIn.php
@@ -9,6 +9,7 @@
 namespace RedboxDigital\Linkedin\Block;
 
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Store\Model\ScopeInterface;
@@ -18,23 +19,39 @@ class LinkedIn extends \Magento\Framework\View\Element\Template
     /** @var ScopeConfigInterface */
     protected $scopeConfig;
 
+    /** @var Session  */
+    protected $customerSession;
+
     /**
      * LinkedIn constructor.
      * @param Template\Context $context
      * @param ScopeConfigInterface $scopeConfig
+     * @param Session $customerSession
      * @param array $data
      */
     public function __construct(
         Template\Context $context,
         ScopeConfigInterface $scopeConfig,
+        Session $customerSession,
         array $data = []
     ) {
         $this->scopeConfig = $scopeConfig;
+        $this->customerSession =$customerSession;
         parent::__construct($context, $data);
     }
 
     public function getConfigValue($path, $scope = ScopeInterface::SCOPE_STORE)
     {
         return $this->scopeConfig->getValue($path, $scope);
+    }
+
+    public function getCustomer()
+    {
+        return $this->customerSession->getCustomer();
+    }
+
+    public function getLinkedinProfile()
+    {
+        return $this->getCustomer()->getLinkedinProfile();
     }
 }
